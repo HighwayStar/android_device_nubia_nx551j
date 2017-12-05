4505645adf344dda6b9505f21f766c8977bee0ed
Billchen1977 <billchen1977@gmail.com>
2017-12-05 22:05:11 +0530

    nx551j: Add btsco playback
    Change-Id: I8c24fda21ee4fc49f1edfb64746172638353a23e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -26,6 +26,8 @@
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    20
 #define BLE_VND_INCLUDED   TRUE
+#define BLUETOOTH_QTI_SW               TRUE
+#define BT_CLEAN_TURN_ON_DISABLED      TRUE
 // skips conn update at conn completion
 #define BTM_SCO_ENHANCED_SYNC_ENABLED  FALSE
 #endif
